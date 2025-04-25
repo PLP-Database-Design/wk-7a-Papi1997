31f4314533687d09e90142723557bd00058c36a5
Papi1997 <lordjarvis02@gmail.com>
2025-04-25 09:25:18 +0300

    Update answers.sql
    Question 1

---
+++ answers.sql
@@ -0,0 +1,20 @@
+
+-- QUESTION 1
+-- Creating the table in 1NF
+
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
+
+
+--  Insert Data into the Table (in 1NF format)
+
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
