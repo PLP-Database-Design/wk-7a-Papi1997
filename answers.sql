bb53e08bd60035f62ab893ef73bb653e028dfd55
Papi1997 <lordjarvis02@gmail.com>
2025-04-25 09:33:34 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -2,7 +2,7 @@
 -- QUESTION 1
 -- Creating the table in 1NF
 
-CREATE TABLE ProductDetail_1NF (
+CREATE TABLE ProductDetail(
     OrderID INT,
     CustomerName VARCHAR(100),
     Product VARCHAR(100)
@@ -11,10 +11,13 @@ CREATE TABLE ProductDetail_1NF (
 
 --  Insert Data into the Table (in 1NF format)
 
-INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+INSERT INTO ProductDetail (OrderID, CustomerName, Product) VALUES
 (101, 'John Doe', 'Laptop'),
 (101, 'John Doe', 'Mouse'),
 (102, 'Jane Smith', 'Tablet'),
 (102, 'Jane Smith', 'Keyboard'),
 (102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
+
+
+
